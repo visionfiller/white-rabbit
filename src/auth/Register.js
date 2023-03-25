d4b21612561f779a740f22ee9fafae8725834321
Vision Filler <visionfiller@gmail.com>
2023-03-25 16:16:41 -0500

    fixed fetch

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { getUsersToCreate, getUserByEmailId, createNewCustomer } from "./UserProvider"
+import { getUsersToCreate, getUserByEmailId} from "./UserProvider"
 
 export const Register = (props) => {
     const [user, setUser] = useState({ isStaff: false })
